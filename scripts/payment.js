4b295043472cc143e240cee0c6910e32c263492d
Nitish Kumar <119393513+CodingDemon1@users.noreply.github.com>
2023-08-25 20:49:19 +0530

    Hotel Management_CS_Frontend

---
+++ scripts/payment.js
@@ -0,0 +1,35 @@
+const baseURL = "https://hotelmanagementcodsoft1.onrender.com";
+
+document.addEventListener("DOMContentLoaded", function (event) {
+	function move() {
+		window.location.href = "index.html";
+	}
+
+	const payment = document.getElementById("payment");
+	payment.addEventListener("click", (event) => {
+		event.preventDefault();
+
+		const bookingId = localStorage.getItem("bookingId");
+		console.log(bookingId);
+		fetch(`${baseURL}/${bookingId}/update-payment`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ paymentStatus: "Paid" }),
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data.message);
+				Swal.fire("Payment Successful", "Booking Confirmed", "success");
+				setTimeout(() => {
+					window.location.assign("index.html");
+				}, 2000);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	});
+});
